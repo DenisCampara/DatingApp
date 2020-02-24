579ac5f329c7e9bc487a7c2c3398b5ec775db623
DenisCampara <deniscampara94@gmail.com>
2020-02-24 16:31:33 +0100

    Migrations

---
+++ DatingApp.API/Migrations/20200221185212_UsersMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class UsersMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Intruduction",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Introduction",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Introduction",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Intruduction",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
