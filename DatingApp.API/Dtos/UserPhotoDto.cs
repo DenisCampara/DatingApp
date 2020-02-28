93869f7257be5195c90e705c0535530d372ad251
DenisCampara <deniscampara94@gmail.com>
2020-02-28 18:36:54 +0100

    Dto

---
+++ DatingApp.API/Dtos/UserPhotoDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserPhotoDto
+    {
+        public string PhotoUrl { get; set; }
+    }
+}
